ee10cf48bfc21c979ea0081712ca254ea6e67580
samin-irtiza <62540152+samin-irtiza@users.noreply.github.com>
2023-05-01 19:43:28 +0600

    Add the edited project

---
+++ src/Schemas/Data.js
@@ -0,0 +1,126 @@
+export const Data = {
+  profile: {
+    name: "Md. Samin Irtiza",
+    ocupation: "Trainee DevOps and Cloud Engineer",
+    location: "Dhaka, Bangladesh",
+    email: "md.saminirtiza@gmail.com",
+    telephone: "+8801748052622",
+    image: "images/90208122_10222269974249078_3631065450546200576_n.jpg",
+  },
+  aboutMe: {
+    label: "Profile",
+    description:
+      "Programmer Analyst, trained to develop computer applications in various technologies and multiple programming languages, applying agile development methodologies, including identification of needs and system designs based on development models.",
+  },
+  skills: {
+    technicalLabel: "Technologies",
+    softLabel: "Skills",
+    technicalSkills: [
+      "React",
+      "Java",
+      "Nodejs",
+      "MySQL",
+      "Git",
+      "Bash",
+      "Python",
+    ],
+    softSkills: [
+      "Analysis capacity",
+      "Problem resolution",
+      "Critical thinking",
+      "Teamwork",
+      "Communication",
+      "Proactivity",
+      "Creativity",
+      "Mentoring",
+    ],
+  },
+  socialMedia: {
+    label: "SOCIAL",
+    social: [
+      {
+        label: "samin-irtiza",
+        name: "linkedin",
+        url: "https://www.linkedin.com/in/samin-irtiza",
+        className: "bxl-linkedin-square",
+      },
+      {
+        label: "samin-irtiza",
+        name: "github",
+        url: "https://github.com/samin-irtiza",
+        className: "bxl-github",
+      },
+      {
+        label: "samin-irtiza",
+        name: "twiiter",
+        url: "https://facebook.com/saminirtiza.arnob",
+        className: "bxl-facebook",
+      },
+    ],
+  },
+  experience: {
+    works: [
+      {
+        title: "Trainee DevOps and Cloud Engineer",
+        period: "Apr. 2023 - Present",
+        company: "BJIT Academy",
+        description: [
+          "A hands on experience on DevOps methodologies and tools covering various popular technologies in software development."
+        ]
+      },
+    ],
+    academic: [
+      {
+        career: "B.Sc. in Computer Science and Engineering",
+        date: "Sep. 2022",
+        institution: "BRAC University",
+      },
+      {
+        career: "H.S.C. in Science",
+        date: "2017",
+        institution: "Rajshahi Government City College",
+      },
+      {
+        career: "S.S.C. in Science",
+        date: "2017",
+        institution: "Rajshahi Government Laboratory High School",
+      },
+    ],
+    proyects: [
+      {
+        name: "React & Django TodoList",
+        project_link:"http://eternull404.pythonanywhere.com",
+        company: "",
+        period: "Feb. 2023",
+        description: [
+          "A simple TodoList App build using React, Django REST and Django frameworks",
+        ],
+      },
+      {
+        name: "Portfolio Website",
+        project_link:"https://samin-irtiza.github.io",
+        company: "Banco Ripley",
+        period: "Feb. 2023",
+        description: [
+          "A portfolio website built with NextJs and Tailwind CSS frameworks"
+        ],
+      },
+      {
+        name: "Analysis of Deep Learning Networks on Low Light Pest Detection",
+        company: "Gobierno de Chile",
+        period: "Aug. 2021 - Sep. 2022",
+        description: [
+          "A comparative analysis of CNN and Transformer based state-of-the-art object detection models on our own custom low-light image dataset.",
+        ],
+      },
+      {
+        name: "Soundscape Discord Music Bot",
+        company: "Gobierno de Chile",
+        period: "2021",
+        description: [
+          "Created a Discord Music bot named ‘Soundscape’ utilizing python asyncio, ffmpeg, youtube-dl and successfully deployed in Heroku (a web hosting platform).",
+        ],
+      },
+    ],
+  },
+};
